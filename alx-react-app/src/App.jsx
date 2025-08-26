9e4bd2791b4cbc906d604cdd3966a5a6ec115c98
youssef <youssef.magdy.office@gmail.com>
2025-08-26 17:54:20 +0200

    one

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import WelcomeMessage from './components/WelcomeMessage'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
