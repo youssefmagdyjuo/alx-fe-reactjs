23a29d50802ab4e43d200b41e7b6c48c7286ab10
youssef <youssef.magdy.office@gmail.com>
2025-09-04 13:24:59 +0200

    context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,13 @@
 import ProfilePage from "./components/ProfilePage";
-import UserContex from "./components/UserContext";
+import UserContext from "./components/UserContext";
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
-      <UserContex.Provider value={userData}>
+      <UserContext.Provider value={userData}>
         <ProfilePage />
-      </UserContex.Provider>
+      </UserContext.Provider>
     </>
   );
 }
