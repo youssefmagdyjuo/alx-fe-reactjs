0db9025c5c9cb3a697d3a36ffee7c6ae198ca62a
youssef <youssef.magdy.office@gmail.com>
2025-09-28 14:43:33 +0200

    task2

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,16 +1,16 @@
 function UserProfile() {
     return (
         <div 
-        className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg"
+        className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg"
         >
         <img 
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto "
         src="https://via.placeholder.com/150" alt="User" />
         <h1 
-        className="text-xl text-blue-800 my-4"
+        className="md:text-xl sm:text-lg text-blue-800 my-4"
         >John Doe</h1>
         <p
-        className="text-gray-600 text-base"
+        className=" md:text-xl sm:text-lg text-gray-600 text-base"
         >Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     );
