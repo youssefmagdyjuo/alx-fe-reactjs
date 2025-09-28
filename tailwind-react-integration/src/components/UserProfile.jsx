64adc024b658f9a41288355176d7090a5d2eafea
youssef <youssef.magdy.office@gmail.com>
2025-09-28 14:48:22 +0200

    task3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
     return (
         <div 
-        className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg"
+        className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl"
         >
         <img 
-        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto "
+        className="hover:scale-110 transition-transform duration-300 ease-in-out rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto "
         src="https://via.placeholder.com/150" alt="User" />
         <h1 
-        className="md:text-xl sm:text-lg text-blue-800 my-4"
+        className="md:text-xl sm:text-lg text-blue-800 hover:text-blue-500 my-4"
         >John Doe</h1>
         <p
         className="sm:text-sm text-gray-600 md:text-base"
