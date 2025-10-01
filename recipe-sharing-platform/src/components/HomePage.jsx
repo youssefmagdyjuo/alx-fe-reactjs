4eaef229bf949691fd265d5d49c699b0b4701273
youssef <youssef.magdy.office@gmail.com>
2025-10-01 22:28:44 +0200

    task1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="max-w-6xl mx-auto p-6 grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="max-w-6xl mx-auto p-6 grid gap-6 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
       {recipes.map((r, index) => (
         <div
           key={index}
