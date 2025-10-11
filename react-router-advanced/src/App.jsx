72a7e01a77b428decc8cb3c4053f40e3e142e6be
youssef <youssef.magdy.office@gmail.com>
2025-10-11 20:03:20 +0200

    task2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,29 +1,28 @@
 import './App.css'
-import { BrowserRouter as Router,Route,Routes,Outlet} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Profile from './components/Profile'
-import ProfileDetails from './components/ProfileDetails'
-import ProfileSettings from './components/ProfileSettings'
 import Home from './components/Home'
 import Login from './components/Login'
 import PrivateRoute from './components/PrivateRoute'
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/Login' element={<Login/>}/>
-          <Route path='/Profile' element={
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+
+        {/* Protected Route */}
+        <Route
+          path='/Profile/*'
+          element={
             <PrivateRoute>
-              <Profile/>
+              <Profile />
             </PrivateRoute>
-            }>
-          <Route path='ProfileDetails/:id' element={<ProfileDetails/>}/>
-          <Route path='ProfileSettings/:id' element={<ProfileSettings/>}/>
-          </Route>
-        </Routes>
-      </Router>
-    </>
+          }
+        />
+      </Routes>
+    </Router>
   )
 }
 
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,17 +1,22 @@
-import { Link ,Outlet} from 'react-router-dom'
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+
 export default function Profile() {
     const id = 5;
+
     return (
         <div>
-        <nav>
-            <Link to={`ProfileDetails/${id}`}>
-            ProfileDetails
-            </Link>|||||
-            <Link to={`ProfileSettings/${id}`}>
-            ProfileSettings
-            </Link>
-        </nav>
-        <Outlet/>
+            <nav>
+                <Link to={`details/${id}`}>Profile Details</Link> ||||
+                <Link to={`settings/${id}`}>Profile Settings</Link>
+            </nav>
+
+            {/* ✅ Nested Routes داخل Profile */}
+            <Routes>
+                <Route path={`details/:id`} element={<ProfileDetails />} />
+                <Route path={`settings/:id`} element={<ProfileSettings />} />
+            </Routes>
         </div>
-    )
+    );
 }
